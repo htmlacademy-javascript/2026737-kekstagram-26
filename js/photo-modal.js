3fc1d417d39ab7c26761e09ea8add2d0d4e9f3fa
Комаров Сергей <serkomarov88@gmail.com>
2022-07-09 15:56:39 +0300

    Добавлена валидация формы загрузки

--- js/photo-modal.js
+++ js/photo-modal.js
@@ -1,26 +1,52 @@
 import { isEscapeKey } from './util.js';
 
+
 const modalCloseButton = document.querySelector('#picture-cancel');
 const fullPhotoModal = document.querySelector('.big-picture');
+
+
 const commentsCountModal = document.querySelector('.social__comment-count');
 const commentsLoaderButton = document.querySelector('.comments-loader');
 
 const modalEscKeyHandler = (evt) => {
   if (isEscapeKey(evt)) {
-    closeModal();
+    closePhotoModal();
   }
 };
 
-function modalOpen () {
+const openPhotoModal = (photo) => {
   fullPhotoModal.classList.remove('hidden');
   commentsCountModal.classList.add('hidden');
   commentsLoaderButton.classList.add('hidden');
   document.body.classList.add('modal-open');
 
-  document.addEventListener('keydown', modalEscKeyHandler);
-}
+  fullPhotoModal.querySelector('.big-picture__img').querySelector('img').src = photo.url;
+  fullPhotoModal.querySelector('.likes-count').textContent = photo.likes;
+  fullPhotoModal.querySelector('.comments-count').textContent = `${photo.comments.length}`;
+  fullPhotoModal.querySelector('.social__caption').textContent = photo.description;
+  const commentsListFragment = document.createDocumentFragment();
 
-function closeModal () {
+  photo.comments.forEach(({avatar, name, message}) => {
+    const comment = document.createElement('li');
+    comment.classList.add('social__comment');
+    const commentAvatar =  document.createElement('img');
+    commentAvatar.classList.add('social__picture');
+    commentAvatar.src = avatar;
+    commentAvatar.alt = name;
+    const commentText = document.createElement('p');
+    commentText.classList.add('social__text');
+    commentText.textContent = message;
+    comment.append(commentAvatar);
+    comment.append(commentText);
+    commentsListFragment.append(comment);
+  });
+
+  fullPhotoModal.querySelector('.social__comments').replaceChildren(commentsListFragment);
+
+  document.addEventListener('keydown', modalEscKeyHandler);
+};
+
+function closePhotoModal () {
   fullPhotoModal.classList.add('hidden');
   commentsCountModal.classList.remove('hidden');
   commentsLoaderButton.classList.remove('hidden');
@@ -29,6 +55,6 @@ function closeModal () {
   document.removeEventListener('keydown', modalEscKeyHandler);
 }
 
-modalCloseButton.addEventListener('click', closeModal);
+modalCloseButton.addEventListener('click', closePhotoModal);
 
-export {modalOpen};
+export {openPhotoModal};
